da36a4e4b629402e283326bdfe34e25bd7beddef
Samucahh <samuelbarcellosluiz@gmail.com>
2022-05-12 22:00:32 -0300

    Enviando alteração Janela01

--- Projeto05-24.04/src/view/Janela01.java
+++ Projeto05-24.04/src/view/Janela01.java
@@ -7,7 +7,7 @@ public class Janela01 extends JFrame {
 	private JPanel contentPane = new JPanel();
 
 	public Janela01() {
-		super("Janela 1");
+		super("Janela 1"); 
 		setBounds(100, 50, 270, 200);
 		getContentPane().setLayout(null);
 	}
