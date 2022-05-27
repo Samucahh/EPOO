0366bc0fc6b4c8899107c7010ccabb725d2b4ece
Samuel Amaral <samuelbarcellosluiz@gmail.com>
2022-05-26 23:12:38 -0300

    Subindo as alterações feitas hoje

---
+++ PetShop-Aula02.05/src/view/CadastroAnimal2.java
@@ -0,0 +1,43 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class CadastroAnimal2 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CadastroAnimal2 window = new CadastroAnimal2();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CadastroAnimal2() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
--- Projeto05-24.04/src/controller/ControllerJanela05.java
+++ Projeto05-24.04/src/controller/ControllerJanela05.java
@@ -3,11 +3,8 @@ package controller;
 import view.Janela05;
 
 public class ControllerJanela05 {
-
-	public class ControleJanela05 {
-		public static void main(String[] args) {
-			Janela05 jan05 = new Janela05();
-			jan05.setVisible(true);
-		}
+	public static void main(String[] args) {
+		Janela05 jan05 = new Janela05();
+		jan05.setVisible(true);
 	}
 }
